9b1bd9f53669e1020a3253e3f7958a3e425336b1
Roy van der Steen <roysteen@live.nl>
2018-06-07 12:38:08 +0200

    Leaderboard update en score patch
    Leaderboard basics added en scoreboard updated.

---
+++ app/src/main/java/nl/bidoofgoo/apps/tables/LeaderBoardActivity.java
@@ -0,0 +1,14 @@
+package nl.bidoofgoo.apps.tables;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class LeaderBoardActivity extends AppCompatActivity {
+
+    public void onCreate(Bundle savedInstanceScore){
+        super.onCreate(savedInstanceScore);
+
+        setContentView(R.layout.activity_leaderboards);
+
+    }
+}
