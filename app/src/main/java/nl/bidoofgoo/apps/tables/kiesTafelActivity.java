aca8b3467f24ad110839e8cd52064d49c376a012
Roy van der Steen <roysteen@live.nl>
2018-06-11 15:59:16 +0200

    kiesTafel fixed
    Terug in de juiste branch zonder conflicten

---
+++ app/src/main/java/nl/bidoofgoo/apps/tables/kiesTafelActivity.java
@@ -0,0 +1,15 @@
+package nl.bidoofgoo.apps.tables;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class kiesTafelActivity extends AppCompatActivity{
+
+    public void onCreate(Bundle savedInstanceScore){
+        super.onCreate(savedInstanceScore);
+
+        setContentView(R.layout.activity_kiestafel);
+
+    }
+
+}
